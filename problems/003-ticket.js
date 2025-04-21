fce29ada9d8c9c85c6772688886187e07a9cbb1a
jusa11 <a.juiceit@gmail.com>
2025-04-21 07:51:06 +0500

    Resolve 003-ticket

--- problems/003-ticket.js
+++ problems/003-ticket.js
@@ -14,7 +14,14 @@
  * @returns {boolean}
  */
 function checkTicket(number) {
-    return undefined;
+    let numberToArray = number.split("");
+
+    let nums1 = numberToArray.slice(0, 3).reduce((acc, num) => acc + +num, 0);
+    let nums2 = numberToArray.slice(3, 6).reduce((acc, num) => acc + +num, 0);
+
+    return nums1 === nums2;
 }
+
+console.log(checkTicket("005212"));
 
 module.exports = checkTicket;
