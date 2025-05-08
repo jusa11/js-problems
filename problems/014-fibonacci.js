7cd3aebc2c9a58de52eb3da2101e2df9f8c4ba0f
jusa11 <a.juiceit@gmail.com>
2025-05-08 06:50:06 +0500

    Resolve 014, 015

--- problems/014-fibonacci.js
+++ problems/014-fibonacci.js
@@ -17,7 +17,22 @@
  * @returns {number}
  */
 function fibonacci(n) {
-    return undefined;
+    if (n === 0 || n === 1) {
+        return n;
+    }
+    let fib1 = 0;
+    let fib2 = 1;
+    let sum;
+
+    for (let i = 1; i < n; i++) {
+        sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = sum;
+    }
+
+    return sum;
 }
+
+console.log(fibonacci(10));
 
 module.exports = fibonacci;
--- problems/015-fibonacci-2.js
+++ problems/015-fibonacci-2.js
@@ -14,7 +14,23 @@
  * @returns {undefined|number}
  */
 function isFibonacci(value) {
-    return undefined;
+    if (value === 0 || value === 1) {
+        return value;
+    }
+    let fib1 = 0;
+    let fib2 = 1;
+    let sum;
+    let i = 1;
+
+    do {
+        sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = sum;
+        ++i;
+    } while (sum < value);
+    return sum === value ? i : undefined;
 }
+
+console.log(isFibonacci(55));
 
 module.exports = isFibonacci;
