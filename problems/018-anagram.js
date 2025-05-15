1cd2205a92aa80d9ad4d7acf249a40365024a05e
jusa11 <a.juiceit@gmail.com>
2025-05-16 02:31:40 +0500

    018 Resolve

--- problems/018-anagram.js
+++ problems/018-anagram.js
@@ -15,7 +15,35 @@
  * @returns {boolean}
  */
 function anagram(x, y) {
-    return undefined;
+    if (x.length !== y.length) {
+        return false;
+    }
+    if (x === "" || y === "") {
+        return false;
+    }
+    const wordX = x.toLocaleLowerCase().split("");
+    const wordY = y.toLocaleLowerCase().split("");
+    let res = true;
+
+    wordX.forEach((letterX, index) => {
+        const countLetterX = wordX.filter((el) => el === letterX);
+        const countLetterY = wordY.filter((el) => el === letterX);
+   
+        if (countLetterX.length !== countLetterY.length) {
+            res = false;
+            return;
+        }
+
+        const checkLetterX = wordY.includes(letterX);
+        const checkLetterY = wordX.includes(wordY[index]);
+        if (!checkLetterX || !checkLetterY) {
+            res = false;
+        }
+    });
+
+    return res;
 }
+
+console.log(anagram("11222", "11122"));
 
 module.exports = anagram;
