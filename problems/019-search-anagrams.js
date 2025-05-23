3383b90158193b20ca03cbe50ff2a795de4c3661
jusa11 <a.juiceit@gmail.com>
2025-05-23 22:56:36 +0500

    incorrect resolve

--- problems/019-search-anagrams.js
+++ problems/019-search-anagrams.js
@@ -12,7 +12,36 @@
  * @returns {string}
  */
 function searchAnagrams(value) {
-    return undefined;
+    if (!value) return "";
+
+    const strToArr = value.split(/[.,? ]+/).filter((el) => el !== "");
+
+    let indexOfAnagram = [];
+
+    strToArr.reduce((acc, word, index) => {
+        const sortWord = word
+            .toLocaleLowerCase()
+            .split("")
+            .filter((symbol) => symbol !== ".");
+        const newWord = sortWord.sort().join("");
+
+        acc.findIndex((word, currentIndex) => {
+            if (word === newWord) {
+                indexOfAnagram = [...indexOfAnagram, currentIndex, index].sort(
+                    (a, b) => a - b
+                );
+            }
+        });
+
+        return [...acc, newWord];
+    }, []);
+
+
+    return indexOfAnagram.map(i => strToArr[i]).join(" ")
+
+
 }
+
+console.log(searchAnagrams("Лист Путь СТИЛ ТУПЬ стиль путь литс тупь"));
 
 module.exports = searchAnagrams;
